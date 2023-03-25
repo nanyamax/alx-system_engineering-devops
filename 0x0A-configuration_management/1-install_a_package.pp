7c90ef29f62a6f754cd85e2b3f3aefb762fdd8f9
nanyamax <ifunanyasunday7@gmail.com>
2023-03-24 18:16:38 -0700

    Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-Puppet { 'flask':
+# installs flask using pip3
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# kills a process
 exec { 'pkill -f killmenow':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
