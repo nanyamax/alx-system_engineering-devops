270e124a4fb7a2142f1e94e90c77bcaeeafa4527
nanyamax <ifunanyasunday7@gmail.com>
2023-08-08 21:58:52 +0300

    api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,14 +6,11 @@ import requests
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
-    params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params,
-        allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title"))
-        for c in results.get("children")]
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        for post in response.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
